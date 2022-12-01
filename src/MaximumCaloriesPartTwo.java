c23a8363939bc6b1e76f672dc7c0d6cb4bf70b95
DESKTOP-0N74O04\Aswin <aswinworkofficial@gmail.com>
2022-12-01 23:31:00 +0000

    AdventOfCode Day 1 Completed

--- src/MaximumCaloriesPartTwo.java
+++ src/MaximumCaloriesPartTwo.java
@@ -13,6 +13,38 @@ public class MaximumCaloriesPartTwo {
         String st;
         Long currentCalorie = 0L, maxCalorie1 = 0L, maxCalorie2 = 0L, maxCalorie3 = 0L;
 
+        while((st = br.readLine()) != null){
+            if(!st.isEmpty())
+                currentCalorie += Long.valueOf(st);
+            else {
+                if(maxCalorie1 == 0)
+                    maxCalorie1 = currentCalorie;
+                else if(maxCalorie1 < currentCalorie){
+                    Long temp = maxCalorie1;
+                    maxCalorie1 = currentCalorie;
+                    maxCalorie2 = temp;
+                }
+                else if(maxCalorie2 == 0){
+                    maxCalorie2 = currentCalorie;
+                }
+                else if(maxCalorie2 < currentCalorie){
+                    Long temp = maxCalorie2;
+                    maxCalorie2 = currentCalorie;
+                    maxCalorie3 = temp;
+                }
+                else if(maxCalorie3 == 0) {
+                    maxCalorie3 = currentCalorie;
+                }
+                else if(maxCalorie3 < currentCalorie){
+                    maxCalorie3 = currentCalorie;
+                }
+                currentCalorie = 0L;
+            }
+        }
+        System.out.println(maxCalorie1);
+        System.out.println(maxCalorie2);
+        System.out.println(maxCalorie3);
+        System.out.println(maxCalorie1 + maxCalorie2 + maxCalorie3);
     }
 
 }
